1e168f583e2527e07ba39b90cf5dff2600872ca6
illoquehambre <illoquehambre@gmail.com>
2022-09-30 10:05:24 +0200

    acta 1 operativa

---
+++ act_01/src/app/formulario/formulario.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  icono='visibility_off'
+  visibilidad="password"
+
+  ngOnInit(): void {
+  }
+
+  changeIcon(){
+    if(this.icono=='visibility_off'){
+      this.icono='visibility'
+      this.visibilidad='text'
+    }else{
+      this.icono='visibility_off'
+      this.visibilidad='password'
+    }
+  }
+  
+
+}
