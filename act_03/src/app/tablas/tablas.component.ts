b78ec099c411ed776648b0909cdc266baf636ead
illoquehambre <illoquehambre@gmail.com>
2022-10-04 11:02:00 +0200

    añadir y eliminar columnas

--- act_03/src/app/tablas/tablas.component.ts
+++ act_03/src/app/tablas/tablas.component.ts
@@ -10,11 +10,24 @@ export class TablasComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'surname', 'dateOfBirth', 'curso'];
   dataSource = TABLA_FAKE;
-
+  binding=true
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  agragarOBorrarColumna(nmb:string){
+    if(this.displayedColumns.includes(nmb)){
+      debugger;
+      var id = this.displayedColumns.indexOf(nmb)
+     
+      this.displayedColumns.splice(id, 1)
+    }else{
+      this.displayedColumns.push(nmb)
+    }
+    
+  
   }
 
   
